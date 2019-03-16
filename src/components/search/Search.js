b9fba8648d7f264bd43e9d0c213ec222e795f2d6
DavidSalami <wizdave97@gmail.com>
2019-03-16 19:14:47 +0100

    project folder structure setup

---
+++ src/components/search/Search.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './search.module.css';
+
+const search = (props) =>{
+  return(
+
+  )
+}
---
+++ src/components/shelf/Shelf.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import classes from './shelf.module.css';
+
+
+class Shelf extends Component {
+  render(){
+    
+  }
+}
+
+export default Shelf;
