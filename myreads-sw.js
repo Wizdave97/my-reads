ebdfa2588057c99cd524b27a4e46e21d432a508f
David Salami <31099392+Wizdave97@users.noreply.github.com>
2019-03-25 22:43:22 +0100

    Update myreads-sw.js

--- myreads-sw.js
+++ myreads-sw.js
@@ -2,9 +2,12 @@ let staticCacheName='my-reads-v1';
 self.addEventListener('install', function(event) {
   urlToCache=[
     './index.html',
-    './static/js/bundle.js',
-    './static/js/0.chunk.js',
-    './static/js/main.chunk.js',
+    './static/js/2.163dad9c.chunk.js',
+    './static/js/2.163dad9c.chunk.js.map',
+    './static/js/main.227dd091.chunk.js',
+    '/static/js/main.227dd091.chunk.js.map',
+    './static/js/runtime~main.464cac55.js',
+    './static/js/runtime~main.464cac55.js.map',
     'https://fonts.googleapis.com/css?family=ZCOOL+XiaoWei',
     'https://fonts.googleapis.com/css?family=Indie+Flower'
   ];
