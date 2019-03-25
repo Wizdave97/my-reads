3cd99f9606764c1b871bd7c887eaccd11951f214
David Salami <31099392+Wizdave97@users.noreply.github.com>
2019-03-25 23:18:21 +0100

    Update myreads-sw.js

--- myreads-sw.js
+++ myreads-sw.js
@@ -26,18 +26,7 @@ self.addEventListener('activate',function(event) {
 });
 
 self.addEventListener('fetch',function(event) {
-  let requestUrl = new URL(event.request.url);
-  
-  if(requestUrl.origin===location.origin){
-    if(requestUrl.pathname==='/my-reads'){
-      console.log(requestUrl.origin);
-      console.log(requestUrl.pathname);
-      event.respondWith(
-        caches.match('/index.html')
-      )
-      return
-    }
-  }
+ 
   event.respondWith(
     caches.match(event.request).then(response=>{
       return response || fetch(event.request)
