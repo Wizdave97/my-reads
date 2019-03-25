2d65f2b2a45a3792678b2c20feec3ddea2327e23
DavidSalami <wizdave97@gmail.com>
2019-03-25 22:04:51 +0100

    Updates

---
+++ myreads-sw.js
@@ -0,0 +1,43 @@
+let staticCacheName='my-reads-v1';
+self.addEventListener('install', function(event) {
+  urlToCache=[
+    '/index.html',
+    '/static/js/bundle.js',
+    '/static/js/0.chunk.js',
+    '/static/js/main.chunk.js',
+    'https://fonts.googleapis.com/css?family=ZCOOL+XiaoWei',
+    'https://fonts.googleapis.com/css?family=Indie+Flower'
+  ];
+  event.waitUntil(caches.open(staticCacheName).then(cache=>{
+    return cache.addAll(urlToCache)
+  }))
+});
+
+self.addEventListener('activate',function(event) {
+  event.waitUntil(
+    caches.keys().then(cacheNames=>{
+      return Promise.all(cacheNames.map(cacheName=>{
+            if(cacheName!==staticCacheName && cacheName.startsWith('my-reads')){
+              caches.delete(cacheName)
+            }
+      }))
+    })
+  )
+});
+
+self.addEventListener('fetch',function(event) {
+  let requestUrl = new URL(event.request.url);
+  if(requestUrl.origin===location.origin){
+    if(requestUrl.pathname==='/'){
+      event.respondWith(
+        caches.match('/index.html')
+      )
+      return
+    }
+  }
+  event.respondWith(
+    caches.match(event.request).then(response=>{
+      return response || fetch(event.request)
+    })
+  )
+})
---
+++ precache-manifest.4f74544b0002943d8e95899f372eb1c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/my-reads/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/my-reads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/my-reads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "0d263240ce2f70cea861",
+    "url": "/my-reads/static/js/runtime~main.464cac55.js"
+  },
+  {
+    "revision": "8d34cf3173926f062ee0",
+    "url": "/my-reads/static/js/main.227dd091.chunk.js"
+  },
+  {
+    "revision": "30397e2b60adc210b166",
+    "url": "/my-reads/static/js/2.163dad9c.chunk.js"
+  },
+  {
+    "revision": "8d34cf3173926f062ee0",
+    "url": "/my-reads/static/css/main.3856eff4.chunk.css"
+  },
+  {
+    "revision": "95cadc4057d4f4c7f9b9e92a9e0341f3",
+    "url": "/my-reads/index.html"
+  }
+];
